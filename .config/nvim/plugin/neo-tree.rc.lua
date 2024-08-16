3018ba71c1ce4e115a455fd9cc7024ea16ce498d
yosukekomagome <96754392+yosukekomagome@users.noreply.github.com>
2024-08-16 15:49:10 +0900

    write setup function

---
+++ .config/nvim/plugin/neo-tree.rc.lua
@@ -0,0 +1,17 @@
+-- setup関数を記述するとneovim起動時にNeoTreeが自動的に起動しちゃうのでコメントにした
+-- require('neo-tree').setup({
+-- 	filesystem = {
+-- 		window = {
+-- 			width = 30, -- ここで幅を設定します。数値は任意で変更可能です。
+-- 		},
+-- 		filtered_items = {
+-- 			visible = false, -- when true, they will just be displayed differently than normal items
+-- 			hide_dotfiles = true,
+-- 			hide_gitignored = true,
+-- 			hide_hidden = true, -- only works on Windows for hidden files/directories
+-- 			hide_by_name = {
+-- 				--"node_modules"
+-- 			},
+-- 		},
+-- 	},
+-- })
