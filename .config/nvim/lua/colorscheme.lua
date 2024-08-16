18cbe4bd0ad15f41accfbbe2790b8fb8809ddbad
yosukekomagome <96754392+yosukekomagome@users.noreply.github.com>
2024-08-16 13:48:37 +0900

    solarized-osaka telescopeBorder highlight setting

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -24,16 +24,31 @@ require('solarized-osaka').setup({
 
 	--- You can override specific color groups to use other groups or a hex color
 	--- function will be called with a ColorScheme table
-	---@param colors ColorScheme
 	on_colors = function(colors) end,
 
 	--- You can override specific highlights to use other groups or a hex color
 	--- function will be called with a Highlights and ColorScheme table
-	---@param highlights Highlights
-	---@param colors ColorScheme
 	on_highlights = function(highlights, colors)
 		highlights.TelescopeBorder = {
-			fg = '#05f1f5',
+			fg = colors.fg_dark,
+		}
+		highlights.TelescopePromptBorder = {
+			fg = colors.fg_dark,
+		}
+		highlights.TelescopePromptTitle = {
+			fg = colors.fg_dark,
+		}
+		highlights.TelescopePromptPrefix = {
+			fg = colors.fg_dark,
+		}
+		highlights.TelescopePromptNormal = {
+			fg = colors.fg_dark,
+		}
+		highlights.TelescopePreviewTitle = {
+			fg = colors.fg_dark,
+		}
+		highlights.TelescopeResultsTitle = {
+			fg = colors.fg_dark,
 		}
 		highlights.LineNr = {
 			fg = '#29a298',
