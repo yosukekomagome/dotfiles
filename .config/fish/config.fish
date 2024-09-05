a80967fa11f4bda08fa20e5fe1b200cbc808fa20
yosukekomagome <96754392+yosukekomagome@users.noreply.github.com>
2024-09-06 08:07:59 +0900

    change fish alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias vim='nvim'
     alias debugchrom='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222'
-    alias ll='eza -l -F --icons --git --sort=type --time-style iso'
-    alias lla='eza -a -l -F --icons --git --sort=type --time-style iso'
+    alias ll='eza -l -F --icons --sort=type --time-style iso'
+    alias lla='eza -a -l -F --icons --sort=type --time-style iso'
 end
 
