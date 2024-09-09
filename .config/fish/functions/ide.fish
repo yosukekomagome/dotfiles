aa80b442d672ac38945789cc3ccae96e309459cf
yosukekomagome <96754392+yosukekomagome@users.noreply.github.com>
2024-09-09 10:17:34 +0900

    add feature ide.fish

--- .config/fish/functions/ide.fish
+++ .config/fish/functions/ide.fish
@@ -1,9 +1,15 @@
 function ide
-  tmux rename -t 0 work
   tmux split-window -v -l 25%
   tmux split-window -h -l 66%
   tmux split-window -h -l 50%
+
   tmux select-pane -t 0
-  nvim .
+
+  set wd (pwd)
+  tmux send-key -t 1 "cd $wd && clear" C-m
+  tmux send-key -t 2 "cd $wd && clear" C-m
+  tmux send-key -t 3 "cd $wd && clear" C-m
+
+  tmux rename -t 0 IDE
 end
 
