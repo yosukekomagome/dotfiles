b8b116ecbee34c564d0894e03bf099c4bfccc5fc
yosukekomagome <96754392+yosukekomagome@users.noreply.github.com>
2024-10-01 14:11:38 +0900

    add new alias/abbr reload command & git log command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    alias vim='nvim'
-    alias debugchrom='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222'
-    alias ll='eza -l -F --icons --sort=type --time-style iso'
-    alias lla='eza -a -l -F --icons --sort=type --time-style iso'
+  # Commands to run in interactive sessions can go here
+  alias vim='nvim'
+  alias debugchrom='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222'
+  alias ll='eza -l -F --icons --sort=type --time-style iso'
+  alias lla='eza -a -l -F --icons --sort=type --time-style iso'
+  abbr -a glog git log --graph --oneline --all
+  abbr -a reload source ~/dotfiles/.config/fish/config.fish
 end
 
