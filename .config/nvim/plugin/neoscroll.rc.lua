dff25af497ff2a2fec00f6b0ef07fbc13c0dfa7e
yosukekomagome <96754392+yosukekomagome@users.noreply.github.com>
2024-10-21 13:53:10 +0900

    delete neoscroll keymapping 'gg' &'G'

--- .config/nvim/plugin/neoscroll.rc.lua
+++ .config/nvim/plugin/neoscroll.rc.lua
@@ -1,16 +1,16 @@
 require('neoscroll').setup({
-  -- Disable <C-e>, <C-y> because I faced a performance issue in a small
-  -- window.
-  mappings = { 'gg','G','<C-u>', '<C-d>', '<C-b>', '<C-f>' },
-  easing_function = "sine",
+	-- Disable <C-e>, <C-y> because I faced a performance issue in a small
+	-- window.
+	-- mappings = { 'gg', 'G', '<C-u>', '<C-d>', '<C-b>', '<C-f>' },
+	mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>' },
+	easing_function = 'sine',
 })
 local t = {}
 
-t["gg"] = { "scroll", { "-2*vim.api.nvim_buf_line_count(0)", "true", "1", [['sine']] } }
-t["G"] = { "scroll", { "2*vim.api.nvim_buf_line_count(0)", "true", "200", [['sine']] } }
+-- t["gg"] = { "scroll", { "-2*vim.api.nvim_buf_line_count(0)", "true", "1", [['sine']] } }
+-- t["G"] = { "scroll", { "2*vim.api.nvim_buf_line_count(0)", "true", "200", [['sine']] } }
 t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '125' } }
 t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '125' } }
-t['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '250' } }
-t['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250' } }
+t['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '125' } }
+t['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '125' } }
 require('neoscroll.config').set_mappings(t)
-
