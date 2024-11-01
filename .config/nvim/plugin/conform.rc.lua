2f6813831f7041f0326a7d43b9c2af926a608be9
yosukekomagome <96754392+yosukekomagome@users.noreply.github.com>
2024-11-01 15:50:42 +0900

    not use prettier

--- .config/nvim/plugin/conform.rc.lua
+++ .config/nvim/plugin/conform.rc.lua
@@ -1,4 +1,4 @@
-local js_formatters = { 'prettierd', 'eslint_d', stop_after_first = false }
+local js_formatters = { 'eslint_d', 'prettierd', stop_after_first = true }
 require('conform').setup({
 	formatters_by_ft = {
 		json = js_formatters,
