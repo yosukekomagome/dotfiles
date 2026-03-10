b08c822ba82c72da29d09557d645d7360fa0f3e0
yosukekomagome <96754392+yosukekomagome@users.noreply.github.com>
2026-03-10 11:10:48 +0900

    refactor:neoscrollの設定変更、カーソルを中央に固定する設定追加

--- .config/nvim/plugin/noice.rc.lua
+++ .config/nvim/plugin/noice.rc.lua
@@ -1,25 +1,29 @@
-require("noice").setup({
-  cmdline = {
-    format = {
-      -- search iconの文字化け対策
-      search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-      search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-    },
-  },
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = false, -- use a classic bottom cmdline for search
-    command_palette = false, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = true, -- add a border to hover docs and signature help
-  },
+require('noice').setup({
+	cmdline = {
+		format = {
+			-- search iconの文字化け対策
+			search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+			search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+		},
+	},
+	lsp = {
+		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		override = {
+			['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+			['vim.lsp.util.stylize_markdown'] = true,
+			-- 以下のコードがneovim編集中に急にサジェストされなくなる現象の原因の様子なのでコメント化2026/02/12実行
+			-- ["cmp.entry.get_documentation"] = true,
+		},
+		signature = {
+			enabled = false,
+		},
+	},
+	-- you can enable a preset for easier configuration
+	presets = {
+		bottom_search = false, -- use a classic bottom cmdline for search
+		command_palette = false, -- position the cmdline and popupmenu together
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		lsp_doc_border = true, -- add a border to hover docs and signature help
+	},
 })
